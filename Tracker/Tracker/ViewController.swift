161a3bc309ab94b6703931cb16aeed70da3cb9ba
Timur Taneev <taneev@yandex.ru>
2023-08-01 19:32:19 +0200

    Project created

---
+++ Tracker/Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Тимур Танеев on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
