02e8cacf15b8041c664a01e85bdade5b027436d1
Timur Taneev <taneev@yandex.ru>
2023-09-21 16:48:56 +0200

    добавлена инициализация YandexMetrica

--- Tracker/Tracker/AppDelegate.swift
+++ Tracker/Tracker/AppDelegate.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import YandexMobileMetrica
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+
+        // Initializing the AppMetrica SDK.
+        let configuration = YMMYandexMetricaConfiguration.init(apiKey: "fcde4204-33c4-4a7e-bce1-a7e8e19fafb7")
+        YMMYandexMetrica.activate(with: configuration!)
+
         window = UIWindow()
         window?.makeKeyAndVisible()
 
